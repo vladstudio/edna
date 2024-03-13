bf8345f9a7fa5e345f421b52f2c395717b22ce43
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-03-13 20:18:22 +0100

    add logging to try figure out crash in prod

--- src/components/App.vue
+++ src/components/App.vue
@@ -50,6 +50,10 @@ export default {
   },
 
   mounted() {
+    let editor = this.$refs.editor;
+    console.log("mounted: editor", editor)
+    console.log("ednaCurrentEditor:", window.ednaCurrentEditor)
+
     window.edna.themeMode.get().then((mode) => {
       this.theme = mode.computed
       this.themeSetting = mode.theme
